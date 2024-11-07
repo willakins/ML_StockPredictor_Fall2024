64f22bfef6022c7c324bbd466f3f2c0ba3df7c95
Alberto Alvarez <alberticoalvarez123@gmail.com>
2024-11-07 02:18:42 -0500

    Implemented random forest model frame
    Apply to data set & test once data is uploaded to repo

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -3,6 +3,13 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../features')))
 from data_preprocessing import main as data_preprocessing_main
+
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, mean_squared_error
+
 """
 Example of how to get cleaned and preprocessed data
 data = data_preprocessing_main()
@@ -12,6 +19,26 @@ This nested dictionary has two keys 'stock_data' and 'news_data' where their val
 """
 def main():
     data = data_preprocessing_main()
+
+    # Features
+    X = data.drop(columns=['target'])
+
+    # Values
+    y = data['target']
+
+    # Split data into training & test sets
+    rand_state = 42 # Use a set random state for reproducible results
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rand_state)  
+
+    # Model, for classification
+    model = RandomForestClassifier(n_estimators=100, random_state=rand_state) # Tune this with hyperparameters once testable
+    model.fit(X_train, y_train) # Train model
+
+    # Evaluate model accuracy using test data set
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    print(f"Model Accuracy: {accuracy}")
 
 if __name__ == '__main__':
     main()
