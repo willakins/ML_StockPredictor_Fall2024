c243cf0234502401de142f0b8491a5da0b9c4135
willakins <willakins23@gmail.com>
2024-12-03 20:24:26 -0500

    Finished the charts

--- src/visualization/results_vis.py
+++ src/visualization/results_vis.py
@@ -1,25 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-# Step 1: Read data from CSV file
+
+
 def create_chart(model_name, companies):
     for company in companies:
-        df = pd.read_csv(f'{model_name}_{company}_predictions.csv')
+        df = pd.read_csv(f'results/{model_name}_{company}_predictions.csv')
+        df['Date'] = df['Date'] - 805
+        df['Actual_sum'] = df['Actual'].cumsum()
+        df['Predicted_sum'] = df['Predicted'].cumsum()
 
-        plt.figure(figsize=(10, 6)) 
-        plt.plot(df['Date'], df['Actual'], label='Actual', color='blue') 
-        plt.plot(df['Date'], df['Predicted'], label='Model Prediction', color='red')
+        #corrected_df = sum_df(df)
+        plt.figure(figsize=(20, 6)) 
+        plt.plot(df['Date'], df['Actual_sum'], label='Actual', color='blue') 
+        plt.plot(df['Date'], df['Predicted_sum'], label='Model Prediction', color='red')
 
         plt.title(f'Accuracy of {model} for {company}') 
         plt.xlabel('Date')
-        plt.ylabel('Market will go up')
+        plt.ylabel('Number of Positive Market Days')
         plt.xticks(rotation=45)
         plt.legend()
 
-        plt.savefig(f'{model}_{company}_chart.png', dpi=300, bbox_inches='tight')
+        plt.savefig(f'docs/charts/{model}_{company}_chart.png', dpi=300, bbox_inches='tight')
+        print(f'Saved chart for {model}')
 
 if __name__ == '__main__':
     companies = ['AAPL', 'GOOG', 'MSFT']
     models = ['svm', 'lstm']
 
+    print('Creating charts...')
     for model in models:
         create_chart(model, companies)
+
+    print('Done!')
